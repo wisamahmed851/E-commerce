f433cfb502351fd14ec12e1597936d6946d564e1
wisamahmed851 <168702325+wisamahmed851@users.noreply.github.com>
2024-12-26 21:08:42 +0500

    added

--- resources/views/ecommerce/dashboard/index.blade.php
+++ resources/views/ecommerce/dashboard/index.blade.php
@@ -6,7 +6,7 @@
         <div class="main-content">
             <!--breadcrumb-->
             <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-                <div class="breadcrumb-title pe-3">Dashboard</div>
+                <div class="breadcrumb-title pe-3">Daaaaashbord</div>
                 <div class="ps-3">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb mb-0 p-0">
